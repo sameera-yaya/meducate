863b610f9aacf1b259d532b413773355c59acf63
Sameera Yayavaram <sameera.yayavaram@csu.fullerton.edu>
2019-02-02 15:42:47 -0500

    Update druginfo.py

--- druginfo.py
+++ druginfo.py
@@ -25,3 +25,51 @@ df = pd.concat([df, df2], ignore_index=True)
 
 df = df.dropna()
 #print(df.dropna())
+
+df = df.dropna().reset_index()
+df.drop(['index'], axis=1,inplace=True)
+#print(df)
+
+def preprocess(text):
+    #text = re.sub('-',' ',text)
+    text = text.lower()
+    tokens = set()
+    for i in range(0,len(text) - 2):
+        shingle = text[i] + text[i+1] + text[i+2]
+        tokens.add(shingle)
+    #tokens = text.split()
+    #tokens = list(text)
+    return tokens
+
+#print(preprocess(df.at[9,'urlDrugName']))
+#print(df.index)
+m = []
+for index in df.index:
+    t = preprocess(df.at[index, 'urlDrugName'])
+    mh = MinHash(num_perm=256)
+    #tlist = str(t)
+    #print(tlist)
+    for d in t:
+        #print(d)
+        mh.update(d.encode('utf8'))
+        #print(mh)
+    m.append(mh)
+    del mh
+
+#print(m)
+#actual_jaccard = float(len(m[14].intersection(m[22])))/float(len(m[14].union(m[22])))
+print(df.index)
+print(df.at[14, 'urlDrugName'])
+print(df.at[22, 'urlDrugName'])
+print(len(df.at[14, 'urlDrugName']))
+print(len(df.at[22, 'urlDrugName']))
+print(m[14].jaccard(m[22]))
+#print(actual_jaccard)
+
+
+lsh = MinHashLSH(threshold=0.95, num_perm=256)
+for minh in range(0, len(m)):
+
+    lsh.insert(df.at[minh, 'urlDrugName'],m[minh])
+result = lsh.query(m[1])
+print(result)
